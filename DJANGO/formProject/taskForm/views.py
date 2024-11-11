b89d37242f2ccba1635c73473a5d9578f6d231a0
a22nicolasmr <a22nicolasmr@iessanclemente.net>
2024-11-11 12:43:04 +0100

    hecho el proyecto 3.10 pero sin probar

--- DJANGO/formProject/taskForm/views.py
+++ DJANGO/formProject/taskForm/views.py
@@ -1,25 +1,51 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 def home(request):
- return render(request, 'generator/home.html')
+    return render(request, 'formProject/form.html')
 
-def results(request):
-    username=request.GET.get('usernameInput')
-    password=request.GET.get('passwordInput')
-    city=request.GET.get('cityInput')
-    select=request.GET.get('select')
-    radio=request.GET.get('status')
+def responsePage(request):
+    username = request.GET.get('usernameInput')
+    password = request.GET.get('passwordInput')
+    city = request.GET.get('cityInput')
+
+    admin = request.GET.get('admin')
+    engineer = request.GET.get('engineer')
+    manager = request.GET.get('manager')
+    guest = request.GET.get('guest')
+
+    options = request.GET.get('select')
+
+    role = ""
+    if admin:
+        role = 'Admin'
+    elif engineer:
+        role = 'Engineer'
+    elif manager:
+        role = 'Manager'
+    elif guest:
+        role = 'Guest'
     
-    mail=request.GET.get("mail")
-    payroll=request.GET.get("payroll")
-    self_service=request.GET.get("self-service")
+    mail = request.GET.get('mail')
+    payroll = request.GET.get('payroll')
+    selfservice = request.GET.get('selfservice')
+
+    signon = ""
     
-    
-    
-    
-    
-    return render(request,'generator/resultsPage.html',{'username':username})
-    
- 
+    if mail:
+        signon = signon + mail
+    elif payroll:
+        signon = signon + " " + payroll
+    elif selfservice:
+        signon = signon + " " + selfservice
+
+    datos = {
+        'username': username,
+        'password': password,
+        'city': city,
+        'role': role,
+        'optionSelected': options,
+        'signon': signon
+    }
+
+    return render(request, 'formProject/responsePage.html', datos)
