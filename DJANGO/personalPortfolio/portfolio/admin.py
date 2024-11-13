169ffc4921681c7b6c9ffc775538586396c3e14f
a22nicolasmr <a22nicolasmr@iessanclemente.net>
2024-11-13 11:02:48 +0100

    seguimos con proyecto portfolio

--- DJANGO/personalPortfolio/portfolio/admin.py
+++ DJANGO/personalPortfolio/portfolio/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+admin.site.register(Project) #nombre de la clase que hicimos en models.py
--- DJANGO/personalPortfolio/portfolio/models.py
+++ DJANGO/personalPortfolio/portfolio/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
-    image=models.ImageField(upload_to='portfolio/images/')
-    url=models.URLField(blank=True)
+    image=models.ImageField(upload_to='portfolio/images/') #directorio para guardar las imagenes
+    url=models.URLField(blank=True) #campos opcionales blank=true
+    
+#cuando creamos una nueva tabla aqui , tenemos que migrar el proyecto para indicar que tenemos una nueva tabla
+
