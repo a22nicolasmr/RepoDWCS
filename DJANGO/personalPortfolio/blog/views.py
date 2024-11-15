5d35df6d50f46a144964c62dc0ec48107f3383d0
a22nicolasmr <a22nicolasmr@iessanclemente.net>
2024-11-15 12:51:39 +0100

    todo ao dia con django hasta poñer o id dos blogs na paxina details

--- DJANGO/personalPortfolio/blog/views.py
+++ DJANGO/personalPortfolio/blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from .models import Blogs
 
 
 def all_blogs(request):
-    blogs=Blogs.objects.all()
+    blogs=Blogs.objects.order_by("-date")
     return render(request,'blogHtmls/all_blogs.html',{"blogs":blogs})
+
+def detail(request,blog_id):
+    blog = get_object_or_404(Blogs,pk=blog_id)
+    return render(request,'blogHtmls/detail.html',{'blog':blog})
