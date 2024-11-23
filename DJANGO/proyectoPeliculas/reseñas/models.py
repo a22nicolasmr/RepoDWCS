c5648b9a29472efa7db24b0971975b6944c2750a
a22nicolasmr <a22nicolasmr@iessanclemente.net>
2024-11-23 14:17:38 +0100

    proyecto django acabado

--- DJANGO/proyectoPeliculas/reseñas/models.py
+++ DJANGO/proyectoPeliculas/reseñas/models.py
@@ -4,5 +4,6 @@ from django.db import models
 
 class Reseñas(models.Model):
     autor=models.CharField(max_length=200)
+    titulo=models.CharField(max_length=150)
     texto=models.TextField(max_length=1000)
     fecha=models.DateField(auto_now_add=True)
