68a3862896ec8b14357d98c76d2078153896703b
a22nicolasmr <a22nicolasmr@iessanclemente.net>
2024-12-02 09:50:55 +0000

    añadido todo desde pythonanywhere

--- DJANGO/personalPortfolioOriginalCopia/personal_portfolio/settings.py
+++ DJANGO/personalPortfolioOriginalCopia/personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*7#fx5a1-f1up!a0j3lvyp4@t=abbo*2(wx8^uxz2&u#200h1g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['a22nicolasmr.pythonanywhere.com']
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
 
