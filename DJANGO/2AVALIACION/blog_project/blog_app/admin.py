e82e51fa5f0c56376012bc0d8981fb5342224b1c
a22nicolasmr <a22nicolasmr@iessanclemente.net>
2025-01-21 12:35:11 +0100

    adasd

--- DJANGO/2AVALIACION/blog_project/blog_app/admin.py
+++ DJANGO/2AVALIACION/blog_project/blog_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Tag, Author, Post
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date', 'author']  # Cambiado a lista
+    list_filter = ['author', 'tag', 'date']  # Cambiado a lista
+    prepopulated_fields = {'slug': ('title',)}  # Corregido el paréntesis
+
+admin.site.register(Tag)
+admin.site.register(Author)
+admin.site.register(Post, PostAdmin)
--- DJANGO/2AVALIACION/blog_project/blog_app/models.py
+++ DJANGO/2AVALIACION/blog_project/blog_app/models.py
@@ -1,28 +1,32 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
-
-class Author(models.Model):
-    first_name=models.CharField(max_length=200)
-    last_name=models.CharField(max_length=200)
-    email=models.CharField(max_length=100)
-    adress=models.CharField(max_length=250)
-    
-    def __str__(self):
-        return f"{self.first_name}, {self.last_name}, {self.email}, {self.adress}"
-    
-class Post(models.Model):
-    first_name=models.CharField(max_length=200)
-    image=models.ImageField()
-    date=models.DateField()
-    slug=models.SlugField()
-    content=models.CharField(max_length=200)
-    
-    def __str__(self):
-        return f"{self.first_name}, {self.image}, {self.date}, {self.slug}, {self.content}"
-
 class Tag(models.Model):
     caption=models.CharField(max_length=200)
     
     def __str__(self):
         return f"{self.caption}"
+    
+class Author(models.Model):
+    first_name=models.CharField(max_length=200)
+    last_name=models.CharField(max_length=200)
+    email_adress=models.EmailField()
+    
+    def __str__(self):
+        return f"{self.first_name}, {self.last_name}, {self.email}, {self.adress}"
+    
+class Post(models.Model):
+    title=models.CharField(max_length=200)
+    excerpt=models.CharField(max_length=200)
+    image=models.ImageField()
+    date=models.DateField(auto_now=True)
+    slug=models.SlugField(unique=True,db_index=True)
+    content=models.TextField(validators=[MinValueValidator(10)])
+    author=models.ForeignKey(Author,null=True,on_delete=models.SET_NULL,related_name="fkposts",blank=True)
+    tag=models.ManyToManyField(Tag)
+    
+    def __str__(self):
+        return f"{self.first_name}, {self.image}, {self.date}, {self.slug}, {self.content}"
+
