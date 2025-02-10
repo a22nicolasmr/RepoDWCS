30e2978d8085f899a7e5e55944ee7b56a1c7a111
a22nicolasmr <a22nicolasmr@iessanclemente.net>
2025-02-10 11:08:29 +0100

    empezada tarea 5.6

---
+++ DJANGO/2AVALIACION/blog_Project_task5.6/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('all-posts/', views.all_posts, name="all_posts"),
+    path('post/<slug:slug>/', views.detail, name='detail'),
+]
